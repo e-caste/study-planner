78c12b93bd26691b5a86611ec31b52e67665466a
e-caste-mbp <en.cas.dev@icloud.com>
2020-08-13 18:10:46 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from PyPDF2 import PdfFileReader
+from cv2 import VideoCapture
+
+
+def get_total_pdf_pages():
+    pass
+
+
+def get_total_video_seconds():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
