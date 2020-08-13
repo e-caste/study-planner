f80409bd88ad26517f7dd86d06841efa98123fa5
e-caste-mbp <en.cas.dev@icloud.com>
2020-08-13 19:56:17 +0200

    fix get_total_video_seconds function by changing library

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sys import argv
 from pathlib import Path
 from PyPDF2 import PdfFileReader
-from cv2 import VideoCapture, CAP_PROP_POS_MSEC
+from pymediainfo import MediaInfo
 
 
 def get_total_pdf_pages(path: str):
@@ -9,14 +9,14 @@ def get_total_pdf_pages(path: str):
 
 
 def get_total_video_seconds(path: str):
-    return sum(VideoCapture(f).get(CAP_PROP_POS_MSEC) for ext in [".mp4", ".flv", ".mov"] for f in Path(path).rglob(f"*{ext}")) / 1000
+    return sum(MediaInfo.parse(f).tracks[0].duration for ext in [".mp4", ".flv", ".mov"] for f in Path(path).rglob(f"*{ext}")) / 1000
 
 
 def main():
     if len(argv) == 0:
         paths = ["."]
     else:
-        paths = [p for p in argv]
+        paths = [p for p in argv[1:]]
 
     result = {
         'pdf_pages': 0,
