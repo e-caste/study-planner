27f37caf3b8230305fb76a098aed982a427c3ae4
e-caste-mbp <en.cas.dev@icloud.com>
2020-08-13 18:18:27 +0200

    add basic script structure

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+from sys import argv, stderr
 from pathlib import Path
 from PyPDF2 import PdfFileReader
 from cv2 import VideoCapture
 
 
-def get_total_pdf_pages():
+def get_total_pdf_pages(path: str):
     pass
 
 
-def get_total_video_seconds():
+def get_total_video_seconds(path: str):
     pass
 
 
 def main():
-    pass
+    if len(argv) == 0:
+        paths = ["."]
+    else:
+        paths = [p for p in argv]
+
+    result = {
+        'pdf_pages': 0,
+        'video_seconds': 0.,
+    }
+    for path in paths:
+        result['pdf_pages'] += get_total_pdf_pages(path)
+        result['video_seconds'] += get_total_video_seconds(path)
+
+    print("stats here")
 
 
 if __name__ == "__main__":
