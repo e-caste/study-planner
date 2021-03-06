cfc04c2a1792c651860db4b73bc68285d5ef2ca1
e-caste-mbp <en.cas.dev@icloud.com>
2021-03-06 21:09:34 +0100

    add Windows version file. ref #28

---
+++ windows_version_file.py
@@ -0,0 +1,46 @@
+from PyInstaller.utils.win32.versioninfo import VSVersionInfo, FixedFileInfo, StringFileInfo, StringTable, \
+    StringStruct, VarFileInfo, VarStruct
+
+# UTF-8
+#
+# For more details about fixed file info 'ffi' see:
+# http://msdn.microsoft.com/en-us/library/ms646997.aspx
+VSVersionInfo(
+    ffi=FixedFileInfo(
+        # filevers and prodvers should be always a tuple with four items: (1, 2, 3, 4)
+        # Set not needed items to zero 0.
+        filevers=(2, 2, 0, 0),
+        prodvers=(2, 2, 0, 0),
+        # Contains a bitmask that specifies the valid bits 'flags'r
+        mask=0x3f,
+        # Contains a bitmask that specifies the Boolean attributes of the file.
+        flags=0x0,
+        # The operating system for which this file was designed.
+        # 0x4 - NT and there is no need to change it.
+        OS=0x4,
+        # The general type of file.
+        # 0x1 - the file is an application.
+        fileType=0x1,
+        # The function of the file.
+        # 0x0 - the function is not defined for this fileType
+        subtype=0x0,
+        # Creation date and time stamp.
+        date=(0, 0)
+    ),
+    kids=[
+        StringFileInfo(
+            [
+                StringTable(
+                    u'040904B0',
+                    [StringStruct(u'CompanyName', u'Enrico Castelli'),
+                     StringStruct(u'FileDescription', u'Study Planner'),
+                     StringStruct(u'FileVersion', u'2.2.0'),
+                     StringStruct(u'InternalName', u'Study Planner'),
+                     StringStruct(u'LegalCopyright', u'Copyright (c) Enrico Castelli'),
+                     StringStruct(u'OriginalFilename', u'main.py'),
+                     StringStruct(u'ProductName', u'Study Planner'),
+                     StringStruct(u'ProductVersion', u'2.2.0')])
+            ]),
+        VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+    ]
+)
