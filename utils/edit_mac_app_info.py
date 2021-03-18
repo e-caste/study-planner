828df3b896875afd821f8ff712bd9951b3bf705c
e-caste-mbp <en.cas.dev@icloud.com>
2021-03-18 12:27:15 +0100

    update version 2.2.1 -> 2.2.2

--- utils/edit_mac_app_info.py
+++ utils/edit_mac_app_info.py
@@ -8,8 +8,8 @@ with open(file_to_edit, 'rb') as f:
 print("Info.plist before editing:", contents)
 
 contents['CFBundleIdentifier'] = 'dev.caste.study-planner'
-contents['CFBundleVersion'] = '2.2.1'
-contents['CFBundleShortVersionString'] = '2.2.1'
+contents['CFBundleVersion'] = '2.2.2'
+contents['CFBundleShortVersionString'] = '2.2.2'
 contents['NSRequiresAquaSystemAppearance'] = False  # enable auto dark mode
 
 print("Info.plist after editing:", contents)
