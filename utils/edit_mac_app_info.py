474dc9855b383430bfc230eab18b6e5a8728957a
e-caste-mbp <git@caste.dev>
2022-05-20 18:13:08 +0200

    bump version 2.2.3->2.2.4

--- utils/edit_mac_app_info.py
+++ utils/edit_mac_app_info.py
@@ -8,8 +8,8 @@ with open(file_to_edit, 'rb') as f:
 print("Info.plist before editing:", contents)
 
 contents['CFBundleIdentifier'] = 'dev.caste.study-planner'
-contents['CFBundleVersion'] = '2.2.3'
-contents['CFBundleShortVersionString'] = '2.2.3'
+contents['CFBundleVersion'] = '2.2.4'
+contents['CFBundleShortVersionString'] = '2.2.4'
 contents['NSRequiresAquaSystemAppearance'] = False  # enable auto dark mode
 
 print("Info.plist after editing:", contents)
